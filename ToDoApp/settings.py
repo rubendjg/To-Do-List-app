3ca5f043c6e695227101d7926151a6995504267e
rubendjg <rdejuangrande@gmail.com>
2025-11-23 16:36:57 +0100

    FIX: Fix to try to solve the problem of th website being marked as insecure

--- ToDoApp/settings.py
+++ ToDoApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_u$sh%peqhxrphis0mw!(5r__yx)#esded0fd_snof##@^x-yk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["rubentodomanager-b0g4hmewcyd8akb9.spaincentral-01.azurewebsites.net", "http://rubentodomanager-b0g4hmewcyd8akb9.spaincentral-01.azurewebsites.net"]
+ALLOWED_HOSTS = ["rubentodomanager-b0g4hmewcyd8akb9.spaincentral-01.azurewebsites.net"]
 
 
 # Application definition
@@ -131,7 +131,8 @@ CSRF_TRUSTED_ORIGINS = [
     "https://rubentodomanager-b0g4hmewcyd8akb9.spaincentral-01.azurewebsites.net",
 ]
 
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# SECURE_SSL_REDIRECT = True
-# CSRF_COOKIE_SECURE = True
-# SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+
